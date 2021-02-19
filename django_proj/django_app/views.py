8dbb957857e2fda932b26d57ae3e23cb541b7e73
Stanislav Khurtesov <s.khurtesov@gmail.com>
2021-02-20 01:57:40 +0300

    Add MySQL query execution example
    Add build script
    Add server execution script

--- django_proj/django_app/views.py
+++ django_proj/django_app/views.py
@@ -3,7 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.conf import settings
+
+import MySQLdb
 
 
 def index(request):
-        return HttpResponse("Hello, world. You're at the polls index.")
+    info = 'MySQL Server Version: '
+
+    db = MySQLdb.connect(
+        host=settings.DATABASES['default']['HOST'], 
+        user=settings.DATABASES['default']['USER'], 
+        password=settings.DATABASES['default']['PASSWORD'], 
+        database=settings.DATABASES['default']['NAME']
+    )
+
+    cursor = db.cursor()
+    cursor.execute("select version()")
+
+    data = cursor.fetchone()
+    info += str(data)
+
+    info = '<h1>' + info + '</h1>'
+    db.close()
+    return HttpResponse(info)
