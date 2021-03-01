4e2573eeddcd338893dff91f0e6bfd677e274788
Stanislav Khurtesov <s.khurtesov@gmail.com>
2021-03-01 21:58:42 +0300

    Fix error handling in configure_mysql.sh

--- configure_mysql.sh
+++ configure_mysql.sh
@@ -3,15 +3,15 @@
 # We need privileges to run mysql service
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "Launch as root to get access to MySQL DB"
-    exit
+    exit 1
 fi
 
 # Trying to start mysql
 service mysql start
-if [ $? -ne 0 ]; then
-    exit_code=$?
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
     echo "[ERROR] Couldn't start mysql service: ${exit_code}" >&2
-    exit
+    exit $exit_code
 fi
 echo "[+] MySQL service started"
 
@@ -23,19 +23,19 @@ DB_user=dbuser
 DB_user_password=123
 
 mysql -u root -e "CREATE DATABASE ${DB_name}";
-if [ $? -ne 0 ]; then
-    exit_code=$?
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
     echo "[ERROR] Couldn't create database ${DB_name}: ${exit_code}" >&2
-    exit
+    exit $exit_code
 fi
 echo "[+] Database ${DB_name} created"
 
 # Create user used in Django project settings
 mysql -u root -e "CREATE USER ${DB_user}@'localhost' IDENTIFIED BY '${DB_user_password}'";
-if [ $? -ne 0 ]; then
-    exit_code=$?
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
     echo "[ERROR] Couldn't create mysql user ${DB_user}: ${exit_code}" >&2
-    exit
+    exit $exit_code
 fi
 echo "[+] User ${DB_user} created"
 
