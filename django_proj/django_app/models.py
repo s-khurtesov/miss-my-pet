869ec0ef66e3eb3de2958c452bc8aa4eada370f5
buntar <svs9119@yandex.ru>
2021-03-07 04:35:47 -0800

    Database created

--- django_proj/django_app/models.py
+++ django_proj/django_app/models.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
 
 # Create your models here.
+#Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(max_length=100, unique=True, primary_key=True)
+    password =  models.CharField(max_length=100)
--- django_proj/django_app/views.py
+++ django_proj/django_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import View, TemplateView
 from django.http import HttpResponse
 from django.conf import settings
+from django.shortcuts import render, redirect
+
+from .models import User
 
 import MySQLdb
 import os
@@ -27,6 +30,73 @@ class PetView(TemplateView):
 
 class RegistrationView(TemplateView):
     template_name = 'user/registration.html'
+
+class RegistrationHandler(TemplateView):
+    template_name = 'user/registration.html'
+
+    def post(self, request, **kwargs):
+        # Company Info
+        register_email = request.POST.get('register_email')
+        register_pass = request.POST.get('register_pass')
+
+        try:
+            user = User.objects.create(
+                email=register_email,
+                password=register_pass
+            )
+            user.save()
+
+            # if user registration successful then say this to him
+            # and redirect him to his new account
+
+            # context = super(RegistrationHandler, self).get_context_data(**kwargs)
+            # context['registration_failed'] = False
+
+            return redirect('/user/account/' + register_email)
+        except:
+            # user registration was unsuccessful
+            # so we need to say this to him
+            # redirection to main page is stub for now
+
+            # context = super(RegistrationHandler, self).get_context_data(**kwargs)
+            # context['registration_failed'] = True
+
+            return redirect('/')
+
+
+class LoginHandler(TemplateView):
+    template_name = 'user/login.html'
+
+    def post(self, request, **kwargs):
+        # Company Info
+        login_email =       request.POST.get('login_email')
+        login_password =    request.POST.get('login_password')
+
+        try:
+            user = User.objects.get(email=login_email)
+            if user.password != login_password:
+                # If there is no user with defined email and password
+                # say to user 'authentication failed'
+
+                # context = super(LoginHandler, self).get_context_data(**kwargs)
+                # context['user_not_exists'] = True
+
+                return redirect('/')
+            else:
+                # Authentication succeed so redirect to user's account page
+
+                # context = super(LoginHandler, self).get_context_data(**kwargs)
+                # context['user_not_exists'] = False
+
+                return redirect('/user/account/' + user.email)
+        except:
+            # If there is no user with defined email
+            # say to user 'authentication failed'
+
+            # context = super(LoginHandler, self).get_context_data(**kwargs)
+            # context['user_not_exists'] = True
+
+            return redirect('/')
 
 
 def index(request):
