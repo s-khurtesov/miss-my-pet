f34849cf26e6660f26eaf0e957f111922345e747
Sheviakova Polina <polinshev00@gmail.com>
2021-04-18 02:42:11 -0700

    Fixed map in create announcement. Fixed some design problems

--- django_proj/django_app/static/js/map_with_polyline.js
+++ django_proj/django_app/static/js/map_with_polyline.js
@@ -1,4 +1,7 @@
-let marker;
+// This example creates an interactive map which constructs a polyline based on
+// user clicks. Note that the polyline only appears once its path property
+// contains two LatLng coordinates.
+let poly;
 let map;
 
 function initMap() {
@@ -7,11 +10,14 @@ function initMap() {
         center: {lat: 59.93863, lng: 30.31413},
     });
 
-    marker = new google.maps.Marker({
-        map: map,
+    poly = new google.maps.Polyline({
+        strokeColor: "#000000",
+        strokeOpacity: 1.0,
+        strokeWeight: 3,
     });
 
     map.setOptions({draggableCursor: 'crosshair'});
+    poly.setMap(map);
 
     // Add a listener for the click event
     map.addListener("click", addLatLng);
@@ -19,11 +25,16 @@ function initMap() {
 
 // Handles click events on a map, and adds a new point to the Polyline.
 function addLatLng(event) {
+    const path = poly.getPath();
 
-    // Add a new marker
-    marker.setMap(null);
-    marker = new google.maps.Marker({
+    // Because path is an MVCArray, we can simply append a new coordinate
+    // and it will automatically appear.
+    path.push(event.latLng);
+
+    // Add a new marker at the new plotted point on the polyline.
+    var marker = new google.maps.Marker({
         position: event.latLng,
+        title: "#" + path.getLength(),
         map: map,
     });
 
